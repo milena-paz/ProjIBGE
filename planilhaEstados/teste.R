0c06a459cbd615c10da2f37349b4244f5c60ab98
milena-paz <milenap4zfreitas@gmail.com>
2025-07-30 07:53:49 -0300

    grafico de dispersao cruzando todas as variaveis (exceto IDH)

--- planilhaEstados/teste.R
+++ planilhaEstados/teste.R
@@ -1,6 +1,6 @@
 dados <- read.csv(file=
 "D:/Documentos (D)/UFJF/6-PERIODO/TCCI/DADOS IBGE/ProjIBGE/planilhaEstados/Dados_Estados_Brasileiros.csv")
 
-par(ps=20)
+png(filename=file.path(getwd(),"dispersao.png"),width=1200,height=1200,pointsize=30)
 plot(dados[,-c(1,3)],cex.axis=0.5)
-boxplot(dados$MortalidadeInfantil/100)
+dev.off()
